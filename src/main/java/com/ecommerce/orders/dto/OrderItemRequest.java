9cd8b032ac58d74b871151cbdd56f83e72cbc3c5
Vishvambhar Rasal <vishvarasal02@gmail.com>
2026-04-27 16:01:05 +0530

    fix: added missing Lombok constructor annotations to OrderItemRequest for test compilation

--- src/main/java/com/ecommerce/orders/dto/OrderItemRequest.java
+++ src/main/java/com/ecommerce/orders/dto/OrderItemRequest.java
@@ -2,11 +2,15 @@ package com.ecommerce.orders.dto;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderItemRequest {
 
     @NotNull(message = "Product ID is required")
